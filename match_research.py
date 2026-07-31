e7b4a4d6e337579b5aea890b8492c337e272a517
FernandesRenata <fernandesdesouza.renata@gmail.com>
2026-07-31 19:24:46 -0300

    Correction of teams sorting

--- match_research.py
+++ match_research.py
@@ -10,15 +10,19 @@ away_team = matches["away_team"].tolist()
 
 teams = home_team + away_team
 
-teams = list(set(teams))
+teams = sorted(list(set(teams)))
 
 for i in range(len(teams)):
     print(f"{i} - {teams[i]}")
 
-team_one = teams[14]
-team_two = teams[0]
+team_one = teams[17]
+team_two = teams[20]
 
-team_one_matches = matches[(matches['home_team'] == team_one) | (matches['away_team'] == team_one)]
-target_matches = team_one_matches[(matches['home_team'] == team_two) | (matches['away_team'] == team_two)]
+target_matches = matches[
+    ((matches['home_team'] == team_one) & (matches['away_team'] == team_two)) |
+    ((matches['home_team'] == team_two) & (matches['away_team'] == team_one))
+]
+
+target_matches =  target_matches['match_id'].tolist()
 
 print(target_matches)
