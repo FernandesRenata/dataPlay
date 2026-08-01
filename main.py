bed175edc24a078d85b90120677ca24e22c9c214
FernandesRenata <fernandesdesouza.renata@gmail.com>
2026-07-31 21:17:20 -0300

    Criation of main

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 from statsbombpy import sb
+
+from match_statistics import match_summary
+from match_selection import match_selection
+from match_research import target_matches
 import  pandas as pd
 
-events = sb.events(match_id=3857256)
-
-summary = pd.DataFrame()
-passes = events[events["type"] == "Pass"]
-shots = events[events["type"] == "Shot"]
-fouls = events[events["type"] == "Foul Committed"]
-on_target = shots[
-    shots["shot_outcome"].isin([
-        "Goal",
-        "Saved",
-        "Saved To Post"
-    ])
-]
-goals = shots[shots["shot_outcome"] == "Goal"]
-corners = passes[
-    passes["pass_type"] == "Corner"
-]
-possessions = (
-    events.groupby("team")["possession"]
-          .nunique()
-)
-xg = (
-    shots.groupby("team")["shot_statsbomb_xg"]
-         .sum()
-)
-accuracy = (
-    passes.assign(correct=passes["pass_outcome"].isna())
-          .groupby("team")["correct"]
-          .mean() * 100
+matches = sb.matches(
+    competition_id=72,
+    season_id=107
 )
 
+team_one, team_two, teams = match_selection(matches)
 
-summary["Pass"] = passes.groupby("team").size()
-summary["Shots"] = shots.groupby("team").size()
-summary["On Target"] = on_target.groupby("team").size()
-summary["Goals"] = goals.groupby("team").size()
-summary["Fouls"] = fouls.groupby("team").size()
-summary["Corners"] = corners.groupby("team").size()
-summary["xG"] = xg
-summary["Pass Accuracy (%)"] = accuracy.round(1)
-summary["Possessions"] = possessions
+match_ids = target_matches(team_one=team_one, team_two=team_two, teams=teams, matches=matches)
 
+print("Choose the match you want to analyze:\n")
 
-print(summary)
+for i, match_id in enumerate(match_ids):
+    partida = matches[matches["match_id"] == match_id].iloc[0]
+
+    print(
+        f"{i} - {partida['match_date']} | "
+        f"{partida['home_team']} x {partida['away_team']}"
+    )
+target_id = int(input("\nChoose the match:\n"))
+target_id = match_ids[target_id]
+
+print(match_summary(target_id))
